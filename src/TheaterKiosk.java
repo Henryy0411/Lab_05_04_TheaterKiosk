b00e1e7f7b164d0879eac78a485864666df65032
giahuytruong <truonggh@mail.uc.edu>
2022-09-23 12:03:55 -0400

    Initial commit

---
+++ src/TheaterKiosk.java
@@ -0,0 +1,11 @@
+public class TheaterKiosk {
+    public static void main(String[] args) {
+        int age= 20;
+
+        if(age >21){
+            System.out.println("You got paper wrist paper" );
+        } else if ( age < 21) {
+            System.out.println("You not enough age for this");
+        }
+    }
+}
